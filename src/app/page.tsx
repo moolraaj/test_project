e299e5a9620ea8e82831ac3198ef61d1034d5be7
raaj73906@gmail.com <neosharma995@gmail.com>
2025-01-17 11:13:58 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@ export default function Home() {
   
   return (
     <>
-    <h1>this is ia home page</h1>
-    <h1>this is ia home page</h1>
-    <h1>this is ia home page</h1>
-    <h1>this is ia home page</h1>
-    <h1>this is ia home page</h1>
-    <h1>this is ia home page</h1>
+    <h1>welcome to the web developemtn</h1>
     </>
   );
 }
