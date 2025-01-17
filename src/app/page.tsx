f59fe8b74798af91c4a49e29e3fdcfa9327ef5d4
moolraaj <tech7raaj@gmail.com>
2025-01-17 07:49:11 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     <h1>this is ia home page</h1>
     <h1>this is ia home page</h1>
     <h1>this is ia home page</h1>
+    <h1>this is ia home page</h1>
     </>
   );
 }
